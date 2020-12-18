00dc9ab1d9db10aa63267ace5e42db01368485e1
psilons <psilons.quanta@gmail.com>
2020-12-18 17:50:32 -0500

    update gunicorn script

--- chapter_03_web_flask/gunicorn_start.sh
+++ chapter_03_web_flask/gunicorn_start.sh
@@ -5,4 +5,7 @@
 # https://martin-thoma.com/flask-gunicorn/
 # https://medium.com/ymedialabs-innovation/deploy-flask-app-with-nginx-using-gunicorn-and-supervisor-d7a93aa07c18
 # # https://medium.com/faun/deploy-flask-app-with-nginx-using-gunicorn-7fda4f50066a
-gunicorn -d --bind 0.0.0.0:5000 src/flask_test_app/gunicorn_main:app
+export CURR_DIR=$(dirname "$0")
+export PYTHONPATH=$CURR_DIR/src
+
+gunicorn --bind 0.0.0.0:8080 flask_test_app.gunicorn_main:app
